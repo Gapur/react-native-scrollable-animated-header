282f15f39f8736e6e8e9f4b1c74d56de5572defe
Gapur Kassym <gapur.kassym@gmail.com>
2020-07-08 22:32:52 +0600

    feat(app): header translate y

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   Image,
   View,
   Text,
+  Animated,
 } from 'react-native';
 import faker from 'faker';
+
+const HEADER_MAX_HEIGHT = 250;
+const HEADER_MIN_HEIGHT = 54;
+const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
 
 const DATA = Array(10)
   .fill(null)
@@ -18,6 +22,36 @@ const DATA = Array(10)
   }));
 
 function App() {
+  const scrollY = useRef(new Animated.Value(0)).current;
+
+  const headerTranslateY = scrollY.interpolate({
+    inputRange: [0, HEADER_SCROLL_DISTANCE],
+    outputRange: [0, -HEADER_SCROLL_DISTANCE + 28],
+    extrapolate: 'clamp',
+  });
+
+  const imageOpacity = scrollY.interpolate({
+    inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+    outputRange: [1, 1, 0],
+    extrapolate: 'clamp',
+  });
+  const imageHeight = scrollY.interpolate({
+    inputRange: [0, HEADER_SCROLL_DISTANCE],
+    outputRange: [0, 100],
+    extrapolate: 'clamp',
+  });
+
+  const titleScale = scrollY.interpolate({
+    inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+    outputRange: [1, 1, 0.9],
+    extrapolate: 'clamp',
+  });
+  const titleTranslateY = scrollY.interpolate({
+    inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+    outputRange: [0, 0, -8],
+    extrapolate: 'clamp',
+  });
+
   const renderListItem = (item) => (
     <View key={item.id} style={styles.card}>
       <Image style={styles.avatar} source={{uri: item.avatar}} />
@@ -27,11 +61,37 @@ function App() {
 
   return (
     <SafeAreaView style={styles.saveArea}>
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={styles.scrollView}>
+      <Animated.ScrollView
+        contentContainerStyle={{paddingTop: HEADER_MAX_HEIGHT - 28}}
+        scrollEventThrottle={16}
+        onScroll={Animated.event(
+          [{nativeEvent: {contentOffset: {y: scrollY}}}],
+          {useNativeDriver: true},
+        )}>
         {DATA.map(renderListItem)}
-      </ScrollView>
+      </Animated.ScrollView>
+      <Animated.View
+        style={[styles.header, {transform: [{translateY: headerTranslateY}]}]}>
+        <Animated.Image
+          style={[
+            styles.headerBackground,
+            {
+              opacity: imageOpacity,
+              transform: [{translateY: imageHeight}],
+            },
+          ]}
+          source={require('./assets/management.jpg')}
+        />
+      </Animated.View>
+      <Animated.View
+        style={[
+          styles.topBar,
+          {
+            transform: [{scale: titleScale}, {translateY: titleTranslateY}],
+          },
+        ]}>
+        <Text style={styles.title}>Management</Text>
+      </Animated.View>
     </SafeAreaView>
   );
 }
@@ -39,9 +99,7 @@ function App() {
 const styles = StyleSheet.create({
   saveArea: {
     flex: 1,
-  },
-  scrollView: {
-    backgroundColor: '#EFF3FB',
+    backgroundColor: '#eff3fb',
   },
   card: {
     flexDirection: 'row',
@@ -60,6 +118,38 @@ const styles = StyleSheet.create({
     marginTop: 12,
     paddingHorizontal: 16,
     paddingVertical: 12,
+  },
+  header: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: '#62d1bc',
+    overflow: 'hidden',
+    height: HEADER_MAX_HEIGHT,
+  },
+  headerBackground: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    width: null,
+    height: HEADER_MAX_HEIGHT,
+    resizeMode: 'cover',
+  },
+  topBar: {
+    marginTop: 40,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+  },
+  title: {
+    color: 'white',
+    fontSize: 20,
   },
   avatar: {
     height: 54,
